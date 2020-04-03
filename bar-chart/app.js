8f08e55bec2b7c262ed2c220c8181f553d82fdf8
Dariusz Wrzesień <dwrzesien@future-processing.com>
2020-04-04 00:28:12 +0200

    barchart

--- bar-chart/app.js
+++ bar-chart/app.js
@@ -3,6 +3,10 @@ d3.json('data/chartData.json').then(result => {
     ready(result);
 });
 
+const svg = d3
+    .select('.bar-chart-container')
+    .append('svg');
+
 // Main function.
 function ready(data) {
     const chartData = filterData(data);
@@ -13,16 +17,31 @@ function ready(data) {
     };
 
     const values = chartData.values;
-    const area = areaDimension();
+    const area = baseDimension();
 
-    drawBase(area)
+    //Scales
+    const yMax = d3.max(values, value => value.y);
+    const yScale = d3
+        .scaleLinear()
+        .domain([0, yMax])
+        .range([0, area.width])
+
+    const xMax = d3.extent(values, date => new Date(date.x))
+    const xScale = d3
+        .scaleLinear()
+        .domain(xMax)
+        .range([0, area.height])
+
+
+    drawBase(area);
+    drawBars(values, xScale, yScale, config);
 }
 
 function filterData(data) {
     return data[0];
 }
 
-function areaDimension() {
+function baseDimension() {
     const margin = {top: 40, right: 40, bottom: 40, left: 40};
     const width = 400 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
@@ -34,13 +53,25 @@ function areaDimension() {
     }
 }
 
-function drawBase(areaDimension) {
-    d3.select('.bar-chart-container')
-        .append('svg')
-        .attr('width', areaDimension.width + areaDimension.margin.left + areaDimension.margin.right)
-        .attr('height', areaDimension.height + areaDimension.margin.top + areaDimension.margin.bottom)
+function drawBase(area) {
+    svg
+        .attr('width', area.width + area.margin.left + area.margin.right)
+        .attr('height', area.height + area.margin.top + area.margin.bottom)
         .append('g')
-        .attr('transform', `translate(${areaDimension.margin.left}, ${areaDimension.margin.top})`);
+        .attr('transform', `translate(${area.margin.left}, ${area.margin.top})`);
+}
+
+function drawBars(chartData, xScale, yScale, config) {
+    const bars = svg
+        .selectAll('.bar')
+        .data(chartData)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', value => yScale(value.y))
+        .attr('width', 20)
+        .attr('height', value => yScale(value.y))
+        .style('fill', config.color)
 }
 
 
